8068ad96f9968a061e9023979899cebb4dcbf16e
+ <+>
2023-02-14 20:36:41 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+  
+  def initialize(height, width, hypotenuse)
+    if height <= 0 || width <= 0 || hypotenuse <= 0
+      raise TriangleError, "Triangle sides must be positive"
+    elsif height + width <= hypotenuse || height + hypotenuse <= width || width + hypotenuse <= height
+      raise TriangleError, "Invalid triangle sides"
+    end
+    @sides = [height, width, hypotenuse]
+  end
+
+  def kind
+    if @sides.uniq.size == 1
+      :equilateral
+    elsif @sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
+
